b2ca183d4b1120dccd2a84d798318cc68d0abc9a
Seokyoung You <seokyoungyu@snu.ac.kr>
2022-04-04 05:04:59 +0900

    Setting: babel, nodemon, and craco

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Routes/Home";
+import { Helmet } from "react-helmet";
+
 function App() {
   return (
-    <div>
-      <h1>독용</h1>
-      <h2>Hello Github</h2>
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Helmet>
+        <title>All About Gis</title>
+      </Helmet>
+      <Switch>
+        <Route path={"/"}>
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
