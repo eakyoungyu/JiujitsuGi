5910e3d50be499c59022767193d186338701a9cb
Seokyoung You <seokyoungyu@snu.ac.kr>
2022-05-10 01:02:22 +0900

    Implement docker and nginx: api data does not work

--- frontend/src/Api/Api.ts
+++ frontend/src/Api/Api.ts
@@ -6,6 +6,7 @@ export interface IBrands {
 }
 
 export function GetBrandsApi() {
-  const apiUrl = "http://127.0.0.1:8000/brand/api/";
+  // const apiUrl = "http://127.0.0.1:8000/brand/api/";
+  const apiUrl = "http://127.0.0.1/brand/api/";
   return fetch(apiUrl).then((res) => res.json());
 }
